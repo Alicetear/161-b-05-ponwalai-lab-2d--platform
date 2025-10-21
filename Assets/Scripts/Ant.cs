583fd3a3d4cfdeaa904c737c41c0e1711e0fc94c
Alicetear <phrnakanth@gmail.com>
2025-10-21 18:52:45 +0700

    EditantwalkAtoB

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -8,16 +8,45 @@ public class Ant : Enemy
     void Start()
     {
         base.Intialize(20);
+
+        DamageHit = 20;
+        velocity = new Vector2(-1.0f , 0.0f);
     }
 
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        if (velocity.x < 0 && rb.position.x <= MovePoint[0].position.x)
+        {
+            Flip();
+        }
+        //move right ?????????????
+        if (velocity.x > 0 && rb.position.x >= MovePoint[1].position.x)
+        {
+            Flip();
+        }
+
     }
+
+    void Flip()
+    {
+        velocity.x *= -1; //change direction of movement
+                          //Flip the image
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Behavior();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(Time.deltaTime);
     }
 }
