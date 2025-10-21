ed0c72596e91517d1dc92e8c8cb573ea2455287b
Alicetear <phrnakanth@gmail.com>
2025-10-21 18:30:06 +0700

    EditAnt

--- Assets/Scripts/Crocodlie.cs
+++ Assets/Scripts/Crocodlie.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Crocodlie : Enemy
+public class Crocodlie : Character
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
