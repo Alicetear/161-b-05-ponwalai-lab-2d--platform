0ac95be3593b19915d71d62e45e581e813ebd667
Alicetear <phrnakanth@gmail.com>
2025-10-28 16:41:34 +0700

    Edit

--- Assets/Scripts/Crocodlie.cs
+++ Assets/Scripts/Crocodlie.cs
@@ -1,17 +1,37 @@
 using UnityEngine;
 
-public class Crocodlie : Enemy
+public class Crocodlie : Enemy, Ishootable
 {
     [SerializeField] float atkRange;
     public Player player;
+
+
+
+    public float ReloadTime { get; set; }
+
+    public float WaitTime { get; set; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Intialize(50);
+        DamageHit = 30;
 
         atkRange = 6.0f;
         player = GameObject.FindFirstObjectByType<Player>();
+
+        WaitTime = 0.0f;
+        ReloadTime = 5.0f;
     }
+
+    private override void FixedUpdate()
+    {
+        WaitTime += WaitTime.fixedDeltaTime;
+        Behavior();
+    }
+
+
+
     public override void Behavior()
     {
         Vector2 distance = transform.position - player.transform.position;
@@ -24,6 +44,14 @@ public class Crocodlie : Enemy
 
     public void Shoot()
     {
+        if (WaitTime >= ReloadTime)
+        {
+            anim.SetTrigger("Shoot");
+            var bullet = Instantiate(bullet, ShootPoint.position, Quaternion.identity);
+            Rock rock = bullet.GetComponent<Rock>();
+            rock.InitWeapon(30, this);
+            WaitTime = 0;
+        }
         Debug.Log($"{this.name} shoots rock to the {player.name}!");
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
 
-public class Player : Character
+public class Player : Character , IShootable
 {
+    [field: SerializeField] public GameObject Bullet {  get; set; }
+
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+
+    public float ReloadTime { get; set; }
+
+    public float WaitTime { get; set; }
+
+
+
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,7 +38,37 @@ public class Player : Character
         // Update is called once per frame
         void Update()
         {
-
+            Shoot();
         }
     }
+
+
+    public void Shoot() 
+    {
+        if (Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime)
+        {
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Banana banana = bullet.GetComponent<Banana>();
+            if (banana != null)
+                banana.InitWeapon(20, this);
+
+            WaitTime = 0.0f;
+            
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+    
 }
