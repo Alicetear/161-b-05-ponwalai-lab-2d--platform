5541e0e96ef47550d95d52abacd13b3a239cf3b9
Alicetear <phrnakanth@gmail.com>
2025-11-02 22:03:41 +0700

    Edit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
 
-public class Player : Character
+public class Player : Character, IShootable
 {
+    [field: SerializeField] public GameObject Bullet { get; set; }
+
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+
+    public float ReloadTime { get; set; }
+
+    public float WaitTime { get; set; }
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Intialize(100);
+        ReloadTime = 1.0f;
+        WaitTime = 0.0f;
     }
 
-    public void OnHitWith(Enemy enemy) 
+    private void FixedUpdate()
+    {
+        WaitTime = Time.fixedDeltaTime;
+    }
+
+    public void OnHitWith(Enemy enemy)
     {
         TakeDamage(enemy.DamageHit);
     }
@@ -24,7 +42,37 @@ public class Player : Character
         // Update is called once per frame
         void Update()
         {
-
+            Shoot();
         }
     }
+
+
+    public void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime)
+        {
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Banana banana = bullet.GetComponent<Banana>();
+            if (banana != null)
+                banana.InitWeapon(20, this);
+
+            WaitTime = 0.0f;
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
